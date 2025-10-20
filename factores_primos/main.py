3c2664a072fbad62352e80496df2eebe24054b40
tomihq <hernandeztomas584@gmail.com>
2025-10-19 22:47:02 -0300

    add: caso primo + caso compuesto

--- factores_primos/main.py
+++ factores_primos/main.py
@@ -3,4 +3,12 @@ def factores_primos(n):
         return []
     elif(n == 2 or n == 3):
         return [n]
-    return [4/2, 4/2]
+    
+    factores_primos_n = []
+    contador = 0
+    while(contador < n):
+        if(n % 2 == 0):
+            factores_primos_n.append(2)
+            n = n/2
+        contador+=1
+    return factores_primos_n
