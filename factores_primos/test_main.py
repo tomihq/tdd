c700e45ca5a31cf308a3a689e9e4dd6ab9c7762a
tomihq <hernandeztomas584@gmail.com>
2025-10-19 22:36:52 -0300

    add: factores primos hacer que falle el primer primo

--- factores_primos/test_main.py
+++ factores_primos/test_main.py
@@ -1,5 +1,8 @@
 from factores_primos.main import factores_primos
 
-def numeros_menores_a_dos_no_tienen_factor_primo():
+def test_numeros_menores_a_dos_no_tienen_factores_primos():
     assert(factores_primos(0) == [])
     assert(factores_primos(1) == [])
+
+def test_numero_2_esta_incluido_en_sus_factores_primos():
+    assert(factores_primos(2) == [2])
