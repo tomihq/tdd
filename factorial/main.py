4c5ed2787e31e586fcb9c2f4a8fb7eccb842db48
tomihq <hernandeztomas584@gmail.com>
2025-10-19 21:35:05 -0300

    add: factorial 2

--- factorial/main.py
+++ factorial/main.py
@@ -1,4 +1,6 @@
 def factorial(n):
     if(n<0):
         return "number must be positive"
-    return 1 
+    if(n == 0 or n == 1):
+        return 1
+    return 2
--- factorial/test_main.py
+++ factorial/test_main.py
@@ -5,3 +5,9 @@ def test_arroja_error_si_es_negativo():
 
 def test_factorial_0_es_1():
     assert(factorial(0) == 1)
+
+def test_factorial_1_es_1():
+    assert(factorial(1) == 1)
+
+def test_factorial_2_es_2():
+    assert(factorial(2) == 2)
